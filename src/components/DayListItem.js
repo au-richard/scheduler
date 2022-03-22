a7fc9bdbc9c13ed6f745dedae21424cdb1eac2c4
Richard <au.richard4@gmail.com>
2022-03-22 15:31:09 -0700

    Added DayListItem test file. Modified DayListItem to pass all tests in test file.

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -9,12 +9,24 @@ export default function DayListItem(props) {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0
   });
+  const formatSpots = () => {
+    // if (props.spots === 0) {
+    //   props.spots = "no";
+    // } else if (props.spots === 1) {
+    // } else {
+    // }
+    
+  }
   return (
     <li 
     className={dayListClass} 
     onClick={() => props.setDay(props.name)}>
       <h2>{props.name}</h2>
-      <h3>{props.spots} spots remaining</h3>
+      <h3>{props.spots === 0 ? "no spots remaining" : props.spots === 1 ? "1 spot remaining" : "2 spots remaining"}</h3>
     </li>
   );
 }
+    //   <h3>{props.spots} spots remaining</h3>
+    //   <h3>{props.spots} spot remaining</h3>
+
+    //   <h3>{props.spots} spots remaining</h3>

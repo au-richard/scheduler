aa09ca4af8ce02a5f4baefcbcbd139b51542c09a
Richard <au.richard4@gmail.com>
2022-03-23 12:33:19 -0700

    Changed formatSpots from inline ternary operator to use formatSpots function with if statements.

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -5,17 +5,29 @@ import "./DayListItem.scss";
 
 
 export default function DayListItem(props) {
+  // const { spots, selected, name, setDay } = props;
+
   const dayListClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0
   });
+
+  const formatSpots = () => {
+    if (props.spots === 0) {
+      return <h3 className="text--light">no spots remaining</h3>;
+    } else if (props.spots === 1) {
+      return <h3 className="text--light">1 spot remaining</h3>;
+    } else if (props.spots > 1) {
+      return <h3 className="text--light">{props.spots} spots remaining</h3>;
+    }
+  } 
   
   return (
     <li 
     className={dayListClass} 
     onClick={() => props.setDay(props.name)}>
-      <h2>{props.name}</h2>
-      <h3>{props.spots === 0 ? "no spots remaining" : props.spots === 1 ? "1 spot remaining" : "2 spots remaining"}</h3>
+      <h2 className="text--regular">{props.name}</h2>
+      {formatSpots()};
     </li>
   );
 }

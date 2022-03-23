990999863ed955508ed7b3fad070d660258f5463
Richard <au.richard4@gmail.com>
2022-03-22 18:10:14 -0700

    Modified setDay to be props.setDay to work with callback function in Application.js.

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,14 +3,13 @@ import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
   const listDays = props.days.map(singleDay => {
-    console.log("single day", singleDay);
     return (
       <DayListItem
         key={singleDay.id}
         name={singleDay.name} 
         spots={singleDay.spots} 
         selected={singleDay.name === props.day}
-        setDay={singleDay.setDay} 
+        setDay={props.setDay} 
       />);
   });
 

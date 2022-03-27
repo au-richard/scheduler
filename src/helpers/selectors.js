c1a87e0465e322c43d03595cd9a0eb58d7125235
Richard <au.richard4@gmail.com>
2022-03-27 14:06:48 -0700

    Added getInterviewersForDay function inside selectors.js. Created test cases based on getAppointmentsForDay.

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -11,15 +11,27 @@ export function getAppointmentsForDay(state, day) {
 }
 
 export function getInterview(state, interview) {
-  console.log("## state", state);
-  console.log("## interview", interview);
+  // console.log("## state", state);
+  // console.log("## interview", interview);
   
   const selectedInterview = {};
   if (interview) {
     selectedInterview["student"] = interview.student;
     selectedInterview["interviewer"] = state.interviewers[interview.interviewer];
-    console.log("## Select Interview", selectedInterview);
+    // console.log("## Select Interview", selectedInterview);
     return selectedInterview;
   }
   return null;
+}
+
+export function getInterviewersForDay(state, day) {
+  if (state.days.length === 0) {
+    return [];
+  }
+  const selectedDay = state.days.find(
+    (interviewers) => interviewers.name === day
+  )
+  if (selectedDay === undefined) return [];
+  const interviewers = selectedDay && selectedDay.interviewers.map((id) => state.interviewers[id]);
+  return interviewers;
 }

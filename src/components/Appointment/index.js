9280dc4cd688dcfeffdeed9651464911e7a1bcf5
Richard <au.richard4@gmail.com>
2022-03-29 13:44:47 -0700

    Added Error Save and Error Delete modes to show Error page when deleting/saving.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,6 +5,7 @@ import Show from "./Show";
 import Form from './Form';
 import Status from './Status';
 import Confirm from './Confirm';
+import Error from './Error';
 import useVisualMode from 'hooks/useVisualMode';
 
 import "./styles.scss";
@@ -18,6 +19,8 @@ export default function Appointment(props) {
   const DELETING = "DELETING";
   const CONFIRM = "CONFIRM";
   const EDIT = "EDIT";
+  const ERROR_SAVE = "ERROR_SAVE";
+  const ERROR_DELETE = "ERROR_DELETE";
   const {mode, transition, back} = useVisualMode(props.interview ? SHOW : EMPTY)
 
   const save = (name, interviewer) => {
@@ -30,14 +33,16 @@ export default function Appointment(props) {
       .then(() => {
         transition(SHOW);
       })
+      .catch(error => transition(ERROR_SAVE, true));
   }
 
   const remove = () => {
-    transition(DELETING);
+    transition(DELETING, true);
     props.cancelInterview(props.id)
       .then(() => {
         transition(EMPTY);
       })
+      .catch(error => transition(ERROR_DELETE, true));
   }
 
   const edit = (props) => {
@@ -95,9 +100,17 @@ export default function Appointment(props) {
         onCancel={back}
       />)}
       {mode === DELETING && (
-        <Status
-          message={"DELETING"}
+        <Status message={"DELETING"}/>
+      )}
+      {mode === ERROR_SAVE && (
+        <Error message={"Could not save appointment."}
+        onClose={back}
       />)}
+      {mode === ERROR_DELETE && (
+        <Error message={"Could not delete appointment."}
+        onClose={back}
+      />)}
+
     </article>
   );
 };
